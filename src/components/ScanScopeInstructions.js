5a1b1a5a7fb60f90792e8c8fac0daf2423158ab2
personryan <mageryan1@gmail.com>
2022-11-16 21:46:15 +0800

    Added the animations

--- src/components/ScanScopeInstructions.js
+++ src/components/ScanScopeInstructions.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import img1 from "../img/endoscope.png";
+import './styles/animation.css';
 
- const ScanScopeInstructions = () => {
+const ScanScopeInstructions = () => {
   return (
     <>
       <Card border="dark" className="text-center mb-2">
         <Card.Body>
           <Card.Title>Place scope on station to scan</Card.Title>
+        </Card.Body>
+      </Card>
+      <Card border="white" className="text-center mb-2">
+        <Card.Body>
+          <img src={img1} alt="endoscopes"></img>
+          <div className="arrow bounce">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="-18 -20 100 100" width="100" height="100" class="bi bi-arrow-down">
+              <path fill-rule="evenodd" d="M32 4a2 2 90 012 2v47.172l12.584-12.588a2 2 90 012.832 2.832l-16 16a2 2 90 01-2.832 0l-16-16a2 2 90 012.832-2.832L30 53.172V6A2 2 90 0132 4z" />
+            </svg>
+          </div>
+          <img src="https://visualpharm.com/assets/806/Table-595b40b65ba036ed117d2585.svg" alt="table" viewBox="-18 -20 100 100" width="100" height="100"></img>
         </Card.Body>
       </Card>
     </>
--- src/sampling/RemoveItemsPage.js
+++ src/sampling/RemoveItemsPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Col, Container, Row, Button, Modal } from 'react-bootstrap'
 import NavigationBar from '../components/NavigationBar'
+import img1 from "../img/endoscope.png";
+import '../components/styles/animation.css';
 
 const RemoveItemsPage = () => {
   const [showModal, setShowModal] = useState(false)
@@ -13,13 +15,13 @@ const RemoveItemsPage = () => {
       return;
     }
 
-    if(timeleft===0) {
+    if (timeleft === 0) {
       setTimeLeft(null)
       handleClose()
       window.location.href = "/";
-   }
-  //  // exit early when we reach 0
-  //  if (!timeleft) return;
+    }
+    //  // exit early when we reach 0
+    //  if (!timeleft) return;
 
     const intervalId = setInterval(() => {
       setTimeLeft(timeleft - 1);
@@ -27,7 +29,7 @@ const RemoveItemsPage = () => {
     return () => clearInterval(intervalId);
 
   }, [timeleft])
-  
+
   return (
     <>
       <NavigationBar active="sample" title="Remove Items" />
@@ -42,13 +44,24 @@ const RemoveItemsPage = () => {
                 <Card.Text>
                   After removing items, you will automatically be redirected back to the home page.
                 </Card.Text>
+                <div class="uparrow bounce">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-18 -20 100 100" width="100" height="100" fill="currentColor"
+                    className="bi bi-arrow-up">
+                    <path fill-rule="evenodd"
+                      d="M32 60a2 2 90 002-2V10.828l12.584 12.588a2 2 90 002.832-2.832l-16-16a2 2 90 00-2.832 0l-16 16a2 2 90 102.832 2.832L30 10.828V58a2 2 90 002 2z" />
+                  </svg>
+                </div>
+                <div>
+                  <img src={img1} alt="endoscopes" className='fade-in-image'></img>
+                </div>
+                <img src="https://visualpharm.com/assets/806/Table-595b40b65ba036ed117d2585.svg" alt="table" viewBox="-18 -20 100 100" width="100" height="100"></img>
               </Card.Body>
             </Card>
           </Col>
         </Row>
         <Row>
           <Col className='text-center'>
-            <Button className="items-removed-button" variant="success" onClick={() => {handleShow(); setTimeLeft(5)}}>Items removed</Button>
+            <Button className="items-removed-button" variant="success" onClick={() => { handleShow(); setTimeLeft(5) }}>Items removed</Button>
           </Col>
         </Row>
         {
